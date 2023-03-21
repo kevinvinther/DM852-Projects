15382dc0b012b1b7c9a385af2013a6abf285914c
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 13:15:22 +0100

    Add test for clear() function

---
+++ asg1/test/clear.cpp
@@ -0,0 +1,12 @@
+// This file test the Tree::clear() function
+#include "test.hpp"
+
+void testClear() {
+  DM852::Tree *clearTree = new DM852::Tree();
+  clearTree->insert(1, "This is the first value");
+  clearTree->insert(2, "This is the 2 value");
+  clearTree->insert(3, "This is the 3 value");
+  clearTree->clear();
+  TEST(clearTree->size() == 0,
+       "The tree did not clear after using the clear() function");
+}
