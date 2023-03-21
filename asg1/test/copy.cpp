465c97f3a66404cabc508d431eb5e076205be00c
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 15:05:41 +0100

    Make copy not pointer, and add second test to make sure it works correctly

--- asg1/test/copy.cpp
+++ asg1/test/copy.cpp
@@ -7,9 +7,12 @@ void testCopyable() {
   DM852::Tree *copyTree2 = new DM852::Tree(*copyTree);
   TEST(*copyTree == *copyTree2, "Two copied trees are not equal.")
 
-  DM852::Tree *copyTree3 = copyTree;
-  TEST(*copyTree3 == *copyTree,
+  DM852::Tree copyTree3 = *copyTree;
+  TEST(copyTree3 == *copyTree,
        "Two copies trees are not equal with = operator");
+
+  copyTree3.insert(2, "This is the second value");
+  TEST(copyTree3 != *copyTree, "Two copied trees are equal after change");
 
   delete copyTree;
   delete copyTree2;
