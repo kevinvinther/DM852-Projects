0726121d9dc5e4b474df48e7aa02359d599f0477
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 12:17:01 +0100

    Add tests for size()

---
+++ asg1/test/size.cpp
@@ -0,0 +1,18 @@
+// File to test the size of the Tree
+#include "test.hpp"
+
+void testSize() {
+  DM852::Tree *sizeTree = new DM852::Tree();
+  TEST(sizeTree->size() == 0,
+       "The size of the Tree should be zero on initialization");
+
+  for (int i = 0; i < 10; i++) {
+    sizeTree->insert(i, std::to_string(i));
+    TEST((sizeTree->size() == (i + 1)),
+         "The size of the tree is wrong after insert.");
+  }
+
+  delete sizeTree;
+  TEST(sizeTree->size() == 0,
+       "The size of the Tree should be zero after delete");
+}
