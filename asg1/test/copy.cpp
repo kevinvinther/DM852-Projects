a4dd2871be63d3d93845a7340d2d887ad4d535b7
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 13:19:04 +0100

    Add copy test

---
+++ asg1/test/copy.cpp
@@ -0,0 +1,12 @@
+// This file tests if copyable is correctl implemented
+#include "test.hpp"
+
+void testCopyable() {
+  DM852::Tree *copyTree = new DM852::Tree();
+  copyTree->insert(1, "This is the first value");
+  DM852::Tree *copyTree2 = new DM852::Tree(*copyTree);
+  TEST(copyTree == copyTree2, "Two copied trees are not equal.")
+
+  DM852::Tree *copyTree3 = copyTree;
+  TEST(copyTree3 == copyTree, "Two copies trees are not equal with = operator");
+}
