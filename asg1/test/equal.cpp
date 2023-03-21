141be21295e5016b2fb880c06747b9ccb9c599d7
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 13:58:26 +0100

    Update files to dereference

--- asg1/test/equal.cpp
+++ asg1/test/equal.cpp
@@ -5,17 +5,17 @@ void testEquality() {
   DM852::Tree *tree1 = new DM852::Tree();
   DM852::Tree *tree2 = new DM852::Tree();
 
-  TEST(tree1 == tree2, "Two empty trees are not equal");
+  TEST(*tree1 == *tree2, "Two empty trees are not equal");
   tree1->insert(1, "This is the first value");
   tree2->insert(1, "This is the first value");
 
-  TEST(tree1 == tree2, "Two trees with the same values are not equal");
+  TEST(*tree1 == *tree2, "Two trees with the same values are not equal");
 
   tree1->insert(2, "This is the 2 value");
 
-  TEST(tree1 != tree2, "Two trees with different values are equal");
+  TEST(*tree1 != *tree2, "Two trees with different values are equal");
 
   tree1->clear();
   tree2->clear();
-  TEST(tree1 == tree2, "Two empty trees are not equal after clear()");
+  TEST(*tree1 == *tree2, "Two empty trees are not equal after clear()");
 }
