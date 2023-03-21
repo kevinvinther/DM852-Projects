e78a44ddc327470b8596c578d862e7ab465027af
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 15:09:22 +0100

    Add output to make it more clear whether or not the tests have passed.

--- asg1/test/runAllTests.cpp
+++ asg1/test/runAllTests.cpp
@@ -1,11 +1,25 @@
 #include "test.hpp"
+#include <iostream>
 
 int main() {
   testClear();
+  std::cout << "TESTS: testClear() passed!" << std::endl;
+  std::cout << "-------------------------" << std::endl;
   testCopyable();
+  std::cout << "TESTS: testCopyable() passed!" << std::endl;
+  std::cout << "-------------------------" << std::endl;
   testFind();
+  std::cout << "TESTS: testFind() passed!" << std::endl;
+  std::cout << "-------------------------" << std::endl;
   testEquality();
+  std::cout << "TESTS: testEquality() passed!" << std::endl;
+  std::cout << "-------------------------" << std::endl;
   testInsert();
+  std::cout << "TESTS: testInsert() passed!" << std::endl;
+  std::cout << "-------------------------" << std::endl;
   testSize();
+  std::cout << "TESTS: testSize() passed!" << std::endl;
+  std::cout << "-------------------------" << std::endl;
+  std::cout << "TESTS: Passed all tests!" << std::endl;
   return 0;
 }
