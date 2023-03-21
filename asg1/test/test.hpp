18562fbfda8f118be61504986ca08dfc3c3b8235
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-21 12:16:50 +0100

    Add test header with test define

---
+++ asg1/test/test.hpp
@@ -0,0 +1,10 @@
+#include "../src/Tree.hpp"
+#include <cassert>
+#include <iostream>
+
+#define TEST(condition, message) assert((condition) && message);
+
+#ifndef TEST_H
+#define TEST_H
+
+#endif // TEST_H
