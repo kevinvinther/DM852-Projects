44661d057b302c7c5dff5863baa5397261d2681c
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-03-22 11:12:50 +0100

    Add part of README to test.hpp, along with reasoning for macro

--- asg1/test/test.hpp
+++ asg1/test/test.hpp
@@ -1,6 +1,12 @@
+/*
+ * This test file defines a macro #define TEST, which is simply a short way to
+ * write a cassert statement.
+ */
 #include "../src/Tree.hpp"
 #include <cassert>
 
+// The reason for using this define, rather than just using assert() is the &&
+// message part, which pops up if an assert fails.
 #define TEST(condition, message) assert((condition) && message);
 
 #ifndef TEST_H
