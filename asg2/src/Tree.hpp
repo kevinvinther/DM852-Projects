0944d8cf48e32b15a0e1925879829b6345fdf984
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-04-05 19:22:24 +0200

    Set up basics for asg2

---
+++ asg2/src/Tree.hpp
@@ -0,0 +1,36 @@
+#include <functional>
+
+namespace DM852 {
+template <typename Key, typename Value, typename Comp = std::less<Key>>
+class Tree {
+public:
+  class iterator {
+  public:
+  private:
+  };
+
+  class const_iterator {
+  public:
+  private:
+  };
+
+  int size() const;   // must run constant time
+  bool empty() const; // must run constant time
+
+  std::pair<iterator, bool> insert(const Key &key, const Value &value);
+  std::pair<iterator, bool> insert(Key &&key, Value &&value);
+
+  iterator find(const Key &key);
+  const_iterator find(const Key &key) const;
+
+  void clear();
+
+  iterator begin();
+  const_iterator begin() const;
+
+  iterator end();
+  const_iterator end() const;
+
+private:
+};
+} // namespace DM852
