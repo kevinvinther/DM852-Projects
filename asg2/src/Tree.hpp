57efa6c6f13bd6ab3d22615bc21e8a26a276d4c8
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-05-08 22:46:40 +0200

    deref

--- asg2/src/Tree.hpp
+++ asg2/src/Tree.hpp
@@ -579,19 +579,19 @@ public:
 
   value_type &front() {
     assert(first_node);
-    return first_node->values->second;
+    return *first_node->values->second;
   }
   const value_type &front() const {
     assert(first_node);
-    return first_node->values->second;
+    return *first_node->values->second;
   }
   value_type &back() {
     assert(last_node);
-    return last_node->values->second;
+    return *last_node->values->second;
   }
   const value_type &back() const {
     assert(last_node);
-    return last_node->values->second;
+    return *last_node->values->second;
   }
 };
 } // namespace DM852
