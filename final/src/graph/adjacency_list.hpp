7a04597c294f3e3f3fe3d231442da7103ae72b7c
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-06-15 15:44:28 +0200

    hello

--- final/src/graph/adjacency_list.hpp
+++ final/src/graph/adjacency_list.hpp
@@ -1,9 +1,9 @@
 #ifndef GRAPH_ADJACENCY_LIST_HPP
 #define GRAPH_ADJACENCY_LIST_HPP
 
+#include "properties.hpp"
 #include "tags.hpp"
 #include "traits.hpp"
-#include "properties.hpp"
 
 #include <boost/iterator/counting_iterator.hpp>
 #include <boost/iterator/filter_iterator.hpp>
@@ -13,162 +13,398 @@
 #include <list>
 #include <vector>
 
-namespace graph {
+namespace graph
+{
 
-template<typename DirectedCategoryT,
-         typename VertexPropT = NoProp,
-         typename EdgePropT = NoProp>
-struct AdjacencyList {
-private:
-	struct OutEdge {
-		// TODO
-	};
+  template <typename DirectedCategoryT, // Task Type (Task 1a-d)
+            typename VertexPropT = NoProp, typename EdgePropT = NoProp>
+  struct AdjacencyList
+  {
+  private:
+    struct OutEdge
+    {
+      OutEdge() = default;
+      OutEdge(std::size_t tar, std::size_t storedEdgeIdx)
+          : tar(tar), storedEdgeIdx(storedEdgeIdx) {}
 
-	using OutEdgeList = std::vector<OutEdge>;
+    public:
+      std::size_t tar;           // target vertex
+      std::size_t storedEdgeIdx; // index of the edge in the edge list
+    };
 
-	struct StoredVertexDirected {
-		OutEdgeList eOut;
-	};
+    struct InEdge
+    {
+      InEdge() = default;
+      InEdge(std::size_t src, std::size_t storedEdgeIdx)
+          : src(src), storedEdgeIdx(storedEdgeIdx) {}
 
-	using StoredVertex = StoredVertexDirected;
+    public:
+      std::size_t src;           // source vertex
+      std::size_t storedEdgeIdx; // index of the edge in the edge list
+    }
 
-	struct StoredEdge {
-		std::size_t src, tar;
-	};
+    using OutEdgeList = std::vector<OutEdge>;
+    using InEdgeList = std::vector<InEdge>;
 
-	using VList = std::vector<StoredVertex>;
-	using EList = std::vector<StoredEdge>;
-public: // Graph
-	using DirectedCategory = DirectedCategoryT;
-	using VertexDescriptor = std::size_t;
+    struct StoredVertexDirected
+    {
+      OutEdgeList eOut;
 
-	struct EdgeDescriptor {
-		EdgeDescriptor() = default;
-		EdgeDescriptor(std::size_t src, std::size_t tar,
-		               std::size_t storedEdgeIdx)
-			: src(src), tar(tar), storedEdgeIdx(storedEdgeIdx) {}
-	public:
-		std::size_t src, tar;
-		std::size_t storedEdgeIdx;
-	public:
-		friend bool operator==(const EdgeDescriptor &a,
-		                       const EdgeDescriptor &b) {
-			return a.storedEdgeIdx == b.storedEdgeIdx;
-		}
-	};
-public: // PropertyGraph
-	using VertexProp = VertexPropT;
-	using EdgeProp = EdgePropT;
-public: // VertexListGraph
-	struct VertexRange {
-		// the iterator is simply a counter that returns its value when
-		// dereferenced
-		using iterator = boost::counting_iterator<VertexDescriptor>;
-	public:
-		VertexRange(std::size_t n) : n(n) {}
-		iterator begin() const { return iterator(0); }
-		iterator end()   const { return iterator(n); }
-	private:
-		std::size_t n;
-	};
-public: // EdgeListGraph
-	struct EdgeRange {
-		// We want to adapt the edge list,
-		// so it dereferences to EdgeDescriptor instead of StoredEdge
-		using EListIterator = typename EList::const_iterator;
+    public:
+      StoredVertexDirected() : eOut(new OutEdgeList) {}
+      StoredVertexDirected(OutEdgeList eOut) : eOut(eOut) {}
+    };
 
-		struct iterator : boost::iterator_adaptor<
-				iterator, // because we use CRTP
-				EListIterator, // the iterator we adapt
-				// we want to convert the StoredEdge into an EdgeDescriptor:
-				EdgeDescriptor,
-				// we can use RA as the underlying iterator supports it:
-				std::random_access_iterator_tag,
-				// when we dereference we return by value, not by reference
-				EdgeDescriptor
-			> {
-			using Base = boost::iterator_adaptor<
-				iterator, EListIterator, EdgeDescriptor,
-				std::random_access_iterator_tag, EdgeDescriptor>;
-		public:
-			iterator() = default;
-			iterator(EListIterator i, EListIterator first) : Base(i), first(first) {}
-		private:
-			// let the Boost machinery use our methods:
-			friend class boost::iterator_core_access;
+    struct StoredVertexBidirectional {
+      OutEdgeList eOut;
+      InEdgeList eIn;
 
-			EdgeDescriptor dereference() const {
-				// get our current position stored in the
-				// boost::iterator_adaptor base class
-				const EListIterator &i = this->base_reference();
-				return EdgeDescriptor{i->src, i->tar,
-					static_cast<std::size_t>(i - first)};
-			}
-		private:
-			EListIterator first;
-		};
-	public:
-		EdgeRange(const AdjacencyList &g) : g(&g) {}
+    public:
+      StoredVertexBidirectional() : eOut(new OutEdgeList), eIn(new InEdgeList) {}
+      StoredVertexBidirectional(OutEdgeList eOut, InEdgeList eIn) : eOut(eOut), eIn(eIn) {}
+    }
 
-		iterator begin() const {
-			return iterator(g->eList.begin(), g->eList.begin());
-		}
+    using StoredVertex = if constexpr (std::is_same(DirecterdCategory, tags::Undirected) || std::is_same(DirectedCategory, tags::Directed)) // constexpr, because is_same is compile time
+        ? StoredVertexDirected
+        : StoredVertexBidirectional;
 
-		iterator end() const {
-			return iterator(g->eList.end(), g->eList.begin());
-		}
-	private:
-		const AdjacencyList *g;
-	};
-public: // IncidenceGraph
-	// TODO
-public: // BidirectionalGraph
-	// TODO
-public:
-	AdjacencyList() = default;
-	AdjacencyList(std::size_t n) : vList(n) {}
-private:
-	VList vList;
-	EList eList;
-public: // Graph
-	friend VertexDescriptor source(EdgeDescriptor e, const AdjacencyList &g) {
-		return e.src;
-	}
+    struct StoredEdge
+    {
+      std::size_t src, tar;
 
-	friend VertexDescriptor target(EdgeDescriptor e, const AdjacencyList &g) {
-		return e.tar;
-	}
-public: // VertexListGraph
-	friend std::size_t numVertices(const AdjacencyList &g) {
-		return g.vList.size();
-	}
+      StoredEdge() = default;
+      StoredEdge(src, tar) : src(src), tar(tar) {}
+    };
 
-	friend VertexRange vertices(const AdjacencyList &g) {
-		return VertexRange(numVertices(g));
-	}
-public: // EdgeListGraph
-	friend std::size_t numEdges(const AdjacencyList &g) {
-		return g.eList.size();
-	}
+    using VList = std::vector<StoredVertex>;
+    using EList = std::vector<StoredEdge>;
 
-	friend EdgeRange edges(const AdjacencyList &g) {
-		return EdgeRange(g);
-	}
-public: // Other
-	friend std::size_t getIndex(VertexDescriptor v, const AdjacencyList &g) {
-		return v;
-	}
-public: // IncidenceGraph
-	// TODO
-public: // BidirectionalGraph
-	// TODO
-public: // MutableGraph
-	// TODO
-public: // MutablePropertyGraph
-	// TODO
-public: // PropertyGraph
-	// TODO
-};
+  public: // Graph
+    using DirectedCategory = DirectedCategoryT;
+    using VertexDescriptor = std::size_t;
+
+    struct EdgeDescriptor
+    {
+      EdgeDescriptor() = default;
+      EdgeDescriptor(std::size_t src, std::size_t tar, std::size_t storedEdgeIdx)
+          : src(src), tar(tar), storedEdgeIdx(storedEdgeIdx) {}
+
+    public:
+      std::size_t src, tar;
+      std::size_t storedEdgeIdx;
+
+    public:
+      friend bool operator==(const EdgeDescriptor &a, const EdgeDescriptor &b)
+      {
+        return a.storedEdgeIdx == b.storedEdgeIdx;
+      }
+    };
+
+  public: // PropertyGraph
+    using VertexProp = VertexPropT;
+    using EdgeProp = EdgePropT;
+
+  public: // VertexListGraph
+    struct VertexRange
+    {
+      // the iterator is simply a counter that returns its value when
+      // dereferenced
+      using iterator = boost::counting_iterator<VertexDescriptor>;
+
+    public:
+      VertexRange(std::size_t n) : n(n) {}
+      iterator begin() const { return iterator(0); }
+      iterator end() const { return iterator(n); }
+
+    private:
+      std::size_t n;
+    };
+
+  public: // EdgeListGraph
+    struct EdgeRange
+    {
+      // We want to adapt the edge list,
+      // so it dereferences to EdgeDescriptor instead of StoredEdge
+      using EListIterator = typename EList::const_iterator;
+
+      struct iterator
+          : boost::iterator_adaptor<
+                iterator,      // because we use CRTP
+                EListIterator, // the iterator we adapt
+                // we want to convert the StoredEdge into an EdgeDescriptor:
+                EdgeDescriptor,
+                // we can use RA as the underlying iterator supports it:
+                std::random_access_iterator_tag,
+                // when we dereference we return by value, not by reference
+                EdgeDescriptor>
+      {
+        using Base =
+            boost::iterator_adaptor<iterator, EListIterator, EdgeDescriptor,
+                                    std::random_access_iterator_tag,
+                                    EdgeDescriptor>;
+
+      public:
+        iterator() = default;
+        iterator(EListIterator i, EListIterator first) : Base(i), first(first) {}
+
+      private:
+        // let the Boost machinery use our methods:
+        friend class boost::iterator_core_access;
+
+        EdgeDescriptor dereference() const
+        {
+          // get our current position stored in the
+          // boost::iterator_adaptor base class
+          const EListIterator &i = this->base_reference();
+          return EdgeDescriptor{i->src, i->tar,
+                                static_cast<std::size_t>(i - first)};
+        }
+
+      private:
+        EListIterator first;
+      };
+
+    public:
+      EdgeRange(const AdjacencyList &g) : g(&g) {}
+
+      iterator begin() const
+      {
+        return iterator(g->eList.begin(), g->eList.begin());
+      }
+
+      iterator end() const { return iterator(g->eList.end(), g->eList.begin()); }
+
+    private:
+      const AdjacencyList *g;
+    };
+
+  public: // IncidenceGraph
+    struct OutEdgeRange
+    {
+      using OutEdgeListIterator = typename OutEdgeList::const_iterator;
+
+      struct iterator : boost::iterator_adaptor<
+                            iterator,                        // because we use CRTP
+                            OutEdgeListIterator,             // the iterator we adapt
+                            EdgeDescriptor,                  // we want to convert the StoredEdge into an EdgeDescriptor
+                            std::bidirectional_iterator_tag, // we can use RA as the underlying iterator supports it
+                            EdgeDescriptor>
+      { // when we dereference we return by value, not by reference
+        using Base = boost::iterator_adaptor(iterator,
+                                             OutEdgeListIterator,
+                                             EdgeDescriptor,
+                                             std::bidirectional_iterator_tag,
+                                             EdgeDescriptor);
+
+      public:
+        iterator() = default;
+        iterator(OutEdgeListIterator i, OutEdgeListIterator first) : Base(i), first(first) {}
+
+      private:
+        friend class boost::iterator_core_access;
+
+        EdgeDescriptor dereference() const
+        {
+          const OutEdgeListIterator &i = this->base_reference();
+          return EdgeDescriptor{i->src, i->tar, static_cast<std::size_t>(i - first)};
+        }
+
+      private:
+        OutEdgeListIterator first;
+      };
+
+    public:
+      OutEdgeRange(VertexDescriptor v, const AdjacencyList &g) : v(v), g(&g) {}
+
+      iterator begin() const {
+          return iterator(g->vList[v].eOut.begin(), g->vList[v].eOut.begin())}
+
+      iterator end() const
+      {
+        return iterator(g->vList[v].eOut.end(), g->vList[v].eOut.begin())
+      }
+
+    private:
+      const AdjacencyList *g;
+      VertexDescriptor v;
+    };
+
+  public: // BidirectionalGraph
+    struct InEdgeRange
+    {
+      using InEdgeListIterator = typename InEdgeList::const_iterator;
+
+      struct iterator : boost::iterator_adaptor<
+                            iterator,                        // because we use CRTP
+                            InEdgeListIterator,              // the iterator we adapt
+                            EdgeDescriptor,                  // we want to convert the StoredEdge into an EdgeDescriptor
+                            std::bidirectional_iterator_tag, // we can use RA as the underlying iterator supports it
+                            EdgeDescriptor>
+      { // when we dereference we return by value, not by reference
+        using Base = boost::iterator_adaptor(iterator,
+                                             InEdgeListIterator,
+                                             EdgeDescriptor,
+                                             std::bidirectional_iterator_tag,
+                                             EdgeDescriptor);
+
+      public:
+        iterator() = default;
+        iterator(InEdgeListIterator i, InEdgeListIterator first) : Base(i), first(first) {}
+
+      private:
+        friend class boost::iterator_core_access;
+
+        EdgeDescriptor dereference() const
+        {
+          const InEdgeListIterator &i = this->base_reference();
+          return EdgeDescriptor{i->src, i->tar, static_cast<std::size_t>(i - first)};
+        }
+
+      private:
+        InEdgeListIterator first;
+      };
+
+    public:
+      InEdgeRange(VertexDescriptor v, const AdjacencyList &g) : v(v), g(&g) {}
+
+      iterator begin() const {
+          return iterator(g->vList[v].eIn.begin(), g->vList[v].eIn.begin())}
+
+      iterator end() const
+      {
+        return iterator(g->vList[v].eIn.end(), g->vList[v].eIn.begin())
+      }
+
+    private:
+      const AdjacencyList *g;
+      VertexDescriptor v;
+
+    } public : AdjacencyList() = default;
+    AdjacencyList(std::size_t n) : vList(n) {}
+
+  private:
+    VList vList;
+    EList eList;
+
+  public: // Graph
+    friend VertexDescriptor source(EdgeDescriptor e, const AdjacencyList &g)
+    {
+      return e.src;
+    }
+
+    friend VertexDescriptor target(EdgeDescriptor e, const AdjacencyList &g)
+    {
+      return e.tar;
+    }
+
+  public: // VertexListGraph
+    friend std::size_t numVertices(const AdjacencyList &g)
+    {
+      return g.vList.size();
+    }
+
+    friend VertexRange vertices(const AdjacencyList &g)
+    {
+      return VertexRange(numVertices(g));
+    }
+
+  public: // EdgeListGraph
+    friend std::size_t numEdges(const AdjacencyList &g) { return g.eList.size(); }
+
+    friend EdgeRange edges(const AdjacencyList &g) { return EdgeRange(g); }
+
+  public: // Other
+    friend std::size_t getIndex(VertexDescriptor v, const AdjacencyList &g)
+    {
+      return v;
+    }
+
+  public: // IncidenceGraph
+    friend OutEdgeRange outEdges(const VertexDescriptor v,
+                                 const AdjacencyList &g)
+
+    {
+      if constexpr (std::is_same(DirectedCategory, tags::Undirected)) // constexpr, because is_same is compile time
+      {
+        // Return a range of the out-edges of v in g
+        return OutEdgeRange(v, g);
+      }
+      else
+      {
+        // Return a range of the incident edges of v in g
+        for (EListIterator it = g.eList.begin(); it != g.eList.end(); ++it)
+        {
+          if (it->src == v || it->tar == v)
+          {
+            // add to inciident list
+          }
+        }
+
+        // Two edges are incident if they share a common vertex
+        return OutEdgeRange(v, g);
+      }
+    }
+
+    friend std::size_t outDegree(const VertexDescriptor v,
+                                 const AdjacencyList &g)
+    {
+      if constexpr (std::is_same(DirectedCategory, tags::Undirected) // constexpr, because is_same is compile time
+      {
+        // Return the number of out-edges of v in g
+        return g.outEdgeList[v].size();
+      }
+      else
+      {
+        // Return the number of incident edges of v in g
+      }
+    }
+
+  public:
+    friend VertexDescriptor addVertex(AdjacencyList g)
+    {
+      // Add a vertex and return a descriptor representing the newly added vertex
+      g.vList.emplace_back();
+      return g.vList.size() -
+             1; // We set a unique id to be equal to the size of the list of
+                // vectors - 1, as it is 0-indexed. Note that this will not work
+                // in case removing a node is supported.
+    }
+    friend EdgeDescriptor addEdge(VertexDescriptor u, VertexDescriptor v,
+                                  VertexDescriptor g)
+    {
+      // Return an edge descriptor representing the newly added edge.
+
+      // Both u and v are valid vertex descriptors for g
+      assert(u <= g.vList.size() && v <= g.vList.size());
+
+      // u and v are different
+      assert(u != v);
+
+      // No edge (u, v) exist already in g
+      for (auto it = g.eList.begin(); it != g.eList.end;
+           ++it)
+      { // use iterator to iterate through each edge
+        assert(it->src != u && it->tar != v);
+      }
+
+      // Put edge into list of edges of graph g
+      g.eList.emplace_back(u, v);
+
+      // return the graph along with the new id
+      return EdgeDescriptor(
+          u, v,
+          g.(eList.size()) - 1); // We set a unique id to be equal to the size of
+                                 // the list of vectors -1, as it is 0-indexed
+    }
+
+  public: // BidirectionalGraph
+          // TODO
+  public: // MutableGraph
+  public: // MutablePropertyGraph
+          // TODO
+  public: // PropertyGraph
+          // TODO
+  };
 
 } // namespace graph
 
