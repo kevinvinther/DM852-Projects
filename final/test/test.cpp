d78e1535e969b615493ddd8bcf3e63d186c430ed
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2023-06-18 15:18:11 +0200

    remove bad test

--- final/test/test.cpp
+++ final/test/test.cpp
@@ -49,29 +49,6 @@ int test_topo_sort () {
     return 0;
 }
 
-int test_dfs() {
-    graph::AdjacencyList<graph::tags::Directed> g(0);
-
-    vertex v1 = addVertex(g);
-    vertex v2 = addVertex(g);
-    vertex v3 = addVertex(g);
-    vertex v4 = addVertex(g);
-    vertex v5 = addVertex(g);
-    vertex v6 = addVertex(g);
-
-    addEdge(v6, v3, g);
-    addEdge(v6, v1, g);
-    addEdge(v5, v1, g);
-    addEdge(v5, v2, g);
-    addEdge(v3, v4, g);
-    addEdge(v4, v2, g);
-
-    std::vector<vertex> vertices(numVertices(g));
-    dfs(g, vertices.begin());
-
-    return 0;
-}
-
 int test_getIndex() {
     graph::AdjacencyList<graph::tags::Directed> g(0);
 
@@ -104,7 +81,6 @@ int main() {
     test_bidirectional_graph_creation();
     test_undirected_graph_creation();
     test_topo_sort();
-    test_dfs();
     test_getIndex();
 
     return 0;
